fc6ff47e710c7b2bede5e7603adf3dc79dd51e69
pablo <pgonzalezalv@users.noreply.github.com>
2016-03-12 22:24:10 +0100

    style: added includes to different files

---
+++ main.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "header.h"
--- mymalloc.c
+++ mymalloc.c
@@ -1,1 +1,8 @@
-ifhzeofzrf
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void *mymalloc(size_t size) {
+
+}
