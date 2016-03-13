b7d472fb32013886804503a5db53e48b2445cf08
Doriane Olewicki <doriane.olewicki@gmail.com>
2016-03-13 15:24:04 +0100

    New .h
    mymalloc.h and myfree.h

---
+++ myfree.h
@@ -0,0 +1,13 @@
+//
+//  myfree.h
+//  
+//
+//  Created by Doriane OLEWICKI on 13/03/16.
+//
+//
+
+#ifndef myfree_h
+#define myfree_h
+
+
+#endif /* myfree_h */
---
+++ mymalloc.h
@@ -0,0 +1,13 @@
+//
+//  mymalloc.h
+//  
+//
+//  Created by Doriane OLEWICKI on 13/03/16.
+//
+//
+
+#ifndef mymalloc_h
+#define mymalloc_h
+
+
+#endif /* mymalloc_h */
