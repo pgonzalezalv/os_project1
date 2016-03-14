741718762c47eaf1596f322c8ab3adaf451472a8
Doriane Olewicki <doriane.olewicki@gmail.com>
2016-03-14 19:49:05 +0100

    free simplified

--- myfree.c
+++ myfree.c
@@ -3,21 +3,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include "myfree.h"
-
 void myfree(void *ptr)
 {
     ptr->alloc = 0 ;
 }
 
-void fusionForwards (block_header here)
-{
-    block_header next = here + here->size + SIZE_BLOCK_HEADER ;
-    if (!next->alloc)
-    {
-        here->size += next->size + SIZE_BLOCK_HEADER ;
-    }
-    return ;
-}
-
-void fusionBackwards (block_header here)
